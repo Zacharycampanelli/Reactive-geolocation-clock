67d3e9383fff937d9044bb440ba4903f09842448
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-11-27 15:54:39 -0500

    useFetch hook created

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,12 @@ export default {
     },
     extend: {
       backgroundImage: {
-        'mobileDaytime': "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('./src/assets/mobile/bg-image-daytime.jpg')"
+        'mobileDaytime': "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('./src/assets/mobile/bg-image-daytime.jpg')",
+        'mobileNighttime': "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('./src/assets/mobile/bg-image-nighttime.jpg')",
+        'tabletDaytime': "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('./src/assets/tablet/bg-image-daytime.jpg')",
+        'tabletNighttime': "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('./src/assets/tablet/bg-image-nighttime.jpg')",
+        'desktopDaytime': "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('./src/assets/desktop/bg-image-daytime.jpg')",
+        'desktopNighttime': "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('./src/assets/desktop/bg-image-nighttime.jpg')"
       }
     }
   },
