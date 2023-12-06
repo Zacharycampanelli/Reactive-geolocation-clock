46d07a82e51f689101c5ca85060b278366f87406
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-12-05 21:27:11 -0500

    fixed greeting component to display correct message and symbol depending on time of day

--- src/assets/icons/IconArrowUp.jsx
+++ src/assets/icons/IconArrowUp.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 const SvgIconArrowUp = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} fill="none" {...props}>
-    <circle cx={16} cy={16} r={16} fill="#303030" />
+    <circle cx={16} cy={16} r={16} fill="#000" />
     <path stroke="#fff" strokeWidth={2} d="m11.2 18.4 4.8-4.8 4.8 4.8" />
   </svg>
 );
--- src/components/Greeting/Greeting.jsx
+++ src/components/Greeting/Greeting.jsx
@@ -5,23 +5,28 @@ import SvgIconMoon from '../../assets/icons/IconMoon';
 
 const Greeting = ({ time }) => {
   const [icon, setIcon] = useState();
-  const [phrase, setPhrase] = useState('')
+  const [phrase, setPhrase] = useState('');
   useEffect(() => {
-    let hour = dayjs(time).format('H');
-    if (5 <= hour < 12) {
-      setIcon(<SvgIconSun/>);
+    // let hour = dayjs(time).format('H');
+    let hour = new Date(time).toTimeString().split(':')[0];
+    hour = Number(hour);
+    if (5 <= hour && hour < 12) {
+      setIcon(<SvgIconSun />);
       setPhrase('Good morning');
-    } else if (12 <= hour < 6) {
-        setIcon(<SvgIconSun/>);      setPhrase('Good afternoon');
-    } else if (6 <= hour < 5) {
-        setIcon(<SvgIconMoon/>);      setPhrase('Good Evening');
+    } else if (12 <= hour && hour < 18) {
+      setIcon(<SvgIconSun />);
+      setPhrase('Good afternoon');
+    } else {
+      setIcon(<SvgIconMoon />);
+      setPhrase('Good Evening');
     }
   }, [time]);
 
-
-  return <div className='flex'>
-    {icon} <span className='ml-4 mb-4 text-[15px] text-white leading-[25px] tracking-[3px] uppercase'>{phrase}</span>
-  </div>;
+  return (
+    <div className="flex">
+      {icon} <span className="ml-4 mb-4 text-[15px] text-white leading-[25px] tracking-[3px] uppercase">{phrase}</span>
+    </div>
+  );
 };
 
 export default Greeting;
