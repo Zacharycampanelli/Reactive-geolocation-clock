53ea210c2201718de9a1fe23e13f0bab69bd37f1
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-12-10 20:56:51 -0500

    drawer color and text update with time of day

--- src/components/ScreenDrawer/ScreenDrawer.jsx
+++ src/components/ScreenDrawer/ScreenDrawer.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Drawer, Button, Typography, IconButton } from '@material-tailwind/react';
 import DrawerButton from '../DrawerButton/DrawerButton';
 import ClockContext from '../../context';
@@ -6,9 +6,10 @@ import { formatTimeZone } from '../../utils/format';
 
 
 export function ScreenDrawer({ expanded, setExpanded }) {
-  const {timeZone, dayOfYear, dayOfWeek, weekNumber} = useContext(ClockContext)
-  const [open, setOpen] = React.useState(false);
-
+  const {timeZone, dayOfYear, dayOfWeek, weekNumber, dayRange} = useContext(ClockContext)
+  const [open, setOpen] = useState(false);
+  const [drawerBackground, setDrawerBackground] = useState('')
+  const [textColor, setTextColor] = useState()
   const openDrawer = () => {
     setOpen(true);
     setExpanded(true);
@@ -16,7 +17,17 @@ export function ScreenDrawer({ expanded, setExpanded }) {
   const closeDrawer = () => {
     setOpen(false);
     setExpanded(false);
-  };
+  }; 
+
+  useEffect(() => {
+    if (dayRange === 'day') {
+      setDrawerBackground('bg-white/75') 
+      setTextColor('text-darkGray') 
+    } else {
+      setDrawerBackground('bg-black/75')  
+      setTextColor('text-white')
+    }
+  },[dayRange])
 
   return (
     <>
@@ -27,24 +38,24 @@ export function ScreenDrawer({ expanded, setExpanded }) {
         size={250}
         open={open}
         onClose={closeDrawer}
-        className="p-4 bg-white/75 backdrop-blur-[20px]"
+        className={`${drawerBackground} p-4  backdrop-blur-[20px]`}
       >
         <div className="flex flex-col items-stretch justify-between mb-6 column flex-column h-[90%] pt-10">
-          <span className="flex justify-between w-[100%]">
-            <p className="text-[10px] text-darkGray leading-[28px] tracking-[2px]">CURRENT TIMEZONE</p>
-            <p className="text-[20px] text-darkGray font-bold">{timeZone !== undefined && formatTimeZone(timeZone)}</p>
+          <span className={`${textColor} flex justify-between w-[100%]`}>
+            <p className="text-[10px] leading-[28px] tracking-[2px]">CURRENT TIMEZONE</p>
+            <p className="text-[20px] font-bold">{timeZone !== undefined && formatTimeZone(timeZone)}</p>
           </span>
-          <span className="flex justify-between w-[100%]">
-            <p className="text-[10px] text-darkGray leading-[28px] tracking-[2px]">DAY OF THE YEAR</p>
-            <p className="text-[20px] text-darkGray font-bold">{dayOfYear}</p>
+          <span className={`${textColor} flex justify-between w-[100%]`}>
+            <p className="text-[10px] leading-[28px] tracking-[2px]">DAY OF THE YEAR</p>
+            <p className="text-[20px] font-bold">{dayOfYear}</p>
           </span>
-          <span className="flex justify-between w-[100%]">
-            <p className="text-[10px] text-darkGray leading-[28px] tracking-[2px]">DAY OF THE WEEK</p>
-            <p className="text-[20px] text-darkGray font-bold">{dayOfWeek}</p>
+          <span className={`${textColor} flex justify-between w-[100%]`}>
+            <p className="text-[10px] leading-[28px] tracking-[2px]">DAY OF THE WEEK</p>
+            <p className="text-[20px] font-bold">{dayOfWeek}</p>
           </span>
-          <span className="flex justify-between w-[100%]">
-            <p className="text-[10px] text-darkGray leading-[28px] tracking-[2px]">WEEK NUMBER</p>
-            <p className="text-[20px] text-darkGray font-bold">{weekNumber}</p>
+          <span className={`${textColor} flex justify-between w-[100%]`}>
+            <p className="text-[10px] leading-[28px] tracking-[2px]">WEEK NUMBER</p>
+            <p className="text-[20px] font-bold">{weekNumber}</p>
           </span>
         </div>
       </Drawer>
