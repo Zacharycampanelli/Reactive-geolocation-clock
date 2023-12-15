66fd490c46527ef1ea33db9d97290b423cc48249
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-12-15 13:42:11 -0500

    new geolocation api added

--- src/components/DisplayCity/DisplayCity.jsx
+++ src/components/DisplayCity/DisplayCity.jsx
@@ -3,8 +3,9 @@ import useFetch from '../../hooks/useFetch';
 
 const DisplayCity = () => {
     const [city, setCity] = useState(null);
+    const [state, setState] = useState(null);
     const [country, setCountry] = useState(null);
-    const ipInfoAPIUrl = `https://api.ipbase.com/v2/info?apikey=${import.meta.env.VITE_APIKEY}`;
+    const ipInfoAPIUrl = `https://api.geoapify.com/v1/ipinfo?apiKey=${import.meta.env.VITE_APIKEY}`;
   
     const { data, isPending, error } = useFetch(ipInfoAPIUrl, null);
 
@@ -12,13 +13,14 @@ const DisplayCity = () => {
     useEffect(() => {
         if (data) {
             console.log(data)
-            setCity(data.data.location.city.name)
-            setCountry(data.data.location.country.ioc)
+            setCity(data.city.name)
+            setState(data.state.name)
+            setCountry(data.country.iso_code)
         }
       }, [data]);
   return (
     <div className='mt-4 mb-12 text-white text-[15px] font-bold leading-[28px] tracking-[3px] uppercase md:text-[18px] md:tracking-[3.6px] lg:24px lg:tracking-[4.8px] lg:inline lg:mt-6'>
-      in {`${city}, ${country}`}
+      in {`${city}, ${state} ${country}`}
     </div>
   )
 }
